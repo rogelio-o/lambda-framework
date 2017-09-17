6df0127fb83b154da83fde1a29e2daf6968cae79
rogelio-o <yo@rogelioorts.com>
2017-09-17 20:35:48 +0200

    Update app tests.

--- test/lambda-framework.spec.ts
+++ test/lambda-framework.spec.ts
@@ -14,7 +14,7 @@ describe('App', () => {
     done()
   });
 
-  it("#init without settings should init with default configuration", async (done) => {
+  it('#init without settings should init with default configuration', async (done) => {
     app.init()
     Object.keys(defaultConfiguration)
       .forEach(param => Chai.expect(defaultConfiguration[param]).to.be.equal(app.get(param)))
@@ -22,7 +22,7 @@ describe('App', () => {
     done();
   });
 
-  it("#init with settings should init with custom configuration", async (done) => {
+  it('#init with settings should init with custom configuration', async (done) => {
     const settings = {}
     settings[Configuration.DEFAULT_MYME_TYPE] = 'text/html'
 
@@ -33,21 +33,21 @@ describe('App', () => {
     done();
   });
 
-  it("#enable should set the param as true", async (done) => {
+  it('#enable should set the param as true', async (done) => {
     app.enable('option1')
     Chai.expect(app.get('option1')).to.be.true
 
     done();
   });
 
-  it("#disable should set the param as false", async (done) => {
+  it('#disable should set the param as false', async (done) => {
     app.disable('option1')
     Chai.expect(app.get('option1')).to.be.false
 
     done();
   });
 
-  it("#set should set the param with the indicated value", async (done) => {
+  it('#set should set the param with the indicated value', async (done) => {
     app.set('option1', 'value1')
     Chai.expect(app.get('option1')).to.be.equal('value1')
 
