b6586ff7c22d52024f2bacf3b937d30e174ce110
rogelio-o <yo@rogelioorts.com>
2017-09-17 19:23:43 +0200

    Changed types files names.

--- src/lib/types/http-error.ts
+++
@@ -1,4 +0,0 @@
-export default interface IHttpError {
-    statusCode: number;
-    cause: Error;
-}
--- src/lib/types/http-handler.ts
+++
@@ -1,7 +0,0 @@
-import IHttpRequest from './http-request'
-import IHttpResponse from './http-response'
-import { Context } from 'aws-lambda'
-
-export default interface IHttpHandler {
-    (req: IHttpRequest, res: IHttpResponse): void;
-}
