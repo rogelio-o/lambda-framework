225996040cfef95e31c4e93f74fd58e4b3aa2008
rogelio-o <yo@rogelioorts.com>
2017-09-17 20:35:38 +0200

    Defined request tests.

--- test/http/request.spec.ts
+++ test/http/request.spec.ts
@@ -2,5 +2,135 @@
  * Test for HttpRequest.
  */
 describe('HttpRequest', () => {
+  it('#headers should return the incoming request headers', async (done) => {
 
+  });
+
+  it('#protocol should return the incoming request protocol', async (done) => {
+
+  });
+
+  it('#secure should return true if the incoming request is secure', async (done) => {
+
+  });
+
+  it('#ip should return the incoming request user ip', async (done) => {
+
+  });
+
+  it('#ip should return the incoming request user ip although the app is through a proxy', async (done) => {
+
+  });
+
+  it('#path should return the incoming request path', async (done) => {
+
+  });
+
+  it('#method should return the incoming request method', async (done) => {
+
+  });
+
+  it('#hostname should return the incoming request hostname', async (done) => {
+
+  });
+
+  it('#xhr should return the incoming request is an AJAX request', async (done) => {
+
+  });
+
+  it('#params should return the intersection between the path, query, body and stage params', async (done) => {
+
+  });
+
+  it('#header should return the value of the header given as argument', async (done) => {
+
+  });
+
+  it('#accepts should return the same type given as argument if it is accepted as response mime type', async (done) => {
+
+  });
+
+  it('#accepts should return false if the type given as argument is not accepted as response mime type', async (done) => {
+
+  });
+
+  it('#accepts should return the preferred type to be used as response mime type between the ones given in the parameter', async (done) => {
+
+  });
+
+  it('#acceptsEncodings should return the same encoding given as argument if it is accepted as response encoding', async (done) => {
+
+  });
+
+  it('#acceptsEncodings should return false if the encoding given as argument is not accepted as response encoding', async (done) => {
+
+  });
+
+  it('#acceptsEncodings should return the preferred encoding to be used as response encoding between the ones given in the parameter', async (done) => {
+
+  });
+
+  it('#acceptsCharsets should return the same charset given as argument if it is accepted as response charset', async (done) => {
+
+  });
+
+  it('#acceptsCharsets should return false if the charset given as argument is not accepted as response charset', async (done) => {
+
+  });
+
+  it('#acceptsCharsets should return the preferred charset to be used as response charset between the ones given in the parameter', async (done) => {
+
+  });
+
+  it('#acceptsLanguages should return the same language given as argument if it is accepted as response language', async (done) => {
+
+  });
+
+  it('#acceptsLanguages should return false if the language given as argument is not accepted as response language', async (done) => {
+
+  });
+
+  it('#acceptsLanguages should return the preferred language to be used as response language between the ones given in the parameter', async (done) => {
+
+  });
+
+  it('#param should return the value of the param given as argument if it is into the path params', async (done) => {
+
+  });
+
+  it('#param should return the value of the param given as argument if it is into the query params', async (done) => {
+
+  });
+
+  it('#param should return the value of the param given as argument if it is into the body params', async (done) => {
+
+  });
+
+  it('#param should return the value of the param given as argument if it is into the stage variables', async (done) => {
+
+  });
+
+  it('#param should return the default value if the value of the param given as argument does not exist', async (done) => {
+
+  });
+
+  it('#is should return true if the types given as parameter are accepted as response mime types', async (done) => {
+
+  });
+
+  it('#fresh should return true if the request is "fresh"', async (done) => {
+
+  });
+
+  it('#fresh should return false if the request is not "fresh"', async (done) => {
+
+  });
+
+  it('#stale should return true if the request is "stale"', async (done) => {
+
+  });
+
+  it('#stale should return false if the request is not "stale"', async (done) => {
+
+  });
 });
