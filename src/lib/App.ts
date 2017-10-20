5df0a93c70c2979d90bc8a6a65d0bd0e76d6dc1a
rogelio-o <yo@rogelioorts.com>
2017-10-20 20:15:17 +0200

    Added capitalized class.

---
+++ src/lib/App.ts
@@ -0,0 +1,126 @@
+import { Callback, Context } from "aws-lambda";
+import configuration from "./configuration/configuration";
+import defaultConfiguration from "./configuration/defaultConfiguration";
+import eventFinalHandler from "./event/eventFinalHandler";
+import EventRequest from "./event/EventRequest";
+import HttpError from "./exceptions/HttpError";
+import httpFinalHandler from "./http/httpFinalHandler";
+import HttpRequest from "./http/HttpRequest";
+import HttpResponse from "./http/HttpResponse";
+import Router from "./Router";
+import IEventHandler from "./types/event/IEventHandler";
+import IEventRequest from "./types/event/IEventRequest";
+import IEventRoutePredicate from "./types/event/IEventRoutePredicate";
+import IHttpHandler from "./types/http/IHttpHandler";
+import IHttpPlaceholderHandler from "./types/http/IHttpPlaceholderHandler";
+import IHttpRequest from "./types/http/IHttpRequest";
+import IHttpResponse from "./types/http/IHttpResponse";
+import IHttpRoute from "./types/http/IHttpRoute";
+import IApp from "./types/IApp";
+import IRouter from "./types/IRouter";
+import { getEventType } from "./utils/utils";
+
+/**
+ * The main object that describes and configures the lambda application.
+ */
+export default class App implements IApp {
+
+  private _settings: object;
+
+  private _router: IRouter;
+
+  constructor() {
+    this._settings = {};
+    this._router = new Router();
+  }
+
+  public init(settings?: object): void {
+    this.initDefaultConfiguration(settings);
+  }
+
+  public enable(key: string): void {
+    this._settings[key] = true;
+  }
+
+  public disable(key: string): void {
+    this._settings[key] = false;
+  }
+
+  public set(key: string, value: any): void {
+    this._settings[key] = value;
+  }
+
+  public get(key: string): any {
+    return this._settings[key];
+  }
+
+  public handle(event: any, context: Context, callback?: Callback): void {
+    const eventType = getEventType(event);
+    if (eventType === "APIGatewayEvent") {
+      const req: IHttpRequest = new HttpRequest(event);
+      const res: IHttpResponse = new HttpResponse(this, req, callback);
+      const done = httpFinalHandler(req, res, {
+        env: this.get(configuration.ENVIRONMENT),
+        onerror: this.logError.bind(this, req)
+      });
+      this._router.httpHandle(req, res, done);
+    } else {
+      const req: IEventRequest = new EventRequest(event);
+      const done = eventFinalHandler(req, {
+        env: this.get(configuration.ENVIRONMENT),
+        onerror: this.logError.bind(this, req)
+      });
+      this._router.eventHandle(req, done);
+    }
+  }
+
+  public use(path?: string, ...handler: IHttpHandler[]): IApp {
+    this._router.use(handler, path);
+
+    return this;
+  }
+
+  public mount(router: IRouter, path?: string): IApp {
+    this._router.mount(router, path);
+
+    return this;
+  }
+
+  public param(name: string, handler: IHttpPlaceholderHandler): IApp {
+    this._router.param(name, handler);
+
+    return this;
+  }
+
+  public route(path: string): IHttpRoute {
+    return this._router.route(path);
+  }
+
+  public event(event: string|IEventRoutePredicate, handler: IEventHandler): IApp {
+    this._router.event(event, handler);
+
+    return this;
+  }
+
+  private initDefaultConfiguration(settings: object): void {
+    this._settings = settings ? settings : defaultConfiguration;
+  }
+
+  private logError(req: IHttpRequest|IEventRequest, err: Error): void {
+    /* istanbul ignore next */
+    if (this.get(configuration.ENVIRONMENT) !== "test") {
+      if (err) {
+        console.error(err instanceof HttpError ? err.cause.message : err.message);
+      } else {
+        if (req instanceof EventRequest) {
+          console.log("No handlers for " + JSON.parse(req.event));
+        } else if (req instanceof HttpRequest) {
+          console.log("No handlers for " + req.method + " " + req.path);
+        } else {
+          console.log("No handlers found.");
+        }
+      }
+    }
+  }
+
+}
---
+++ src/lib/Router.ts
@@ -0,0 +1,274 @@
+import EventLayer from "./event/EventLayer";
+import EventRouterExecutor from "./event/EventRouterExecutor";
+import HttpLayer from "./http/HttpLayer";
+import HttpRoute from "./http/HttpRoute";
+import HttpRouterExecutor from "./http/HttpRouterExecutor";
+import IEventHandler from "./types/event/IEventHandler";
+import IEventLayer from "./types/event/IEventLayer";
+import IEventRequest from "./types/event/IEventRequest";
+import IEventRoutePredicate from "./types/event/IEventRoutePredicate";
+import IEventRouterExecutor from "./types/event/IEventRouterExecutor";
+import IHttpHandler from "./types/http/IHttpHandler";
+import IHttpLayer from "./types/http/IHttpLayer";
+import IHttpPlaceholderHandler from "./types/http/IHttpPlaceholderHandler";
+import IHttpRequest from "./types/http/IHttpRequest";
+import IHttpResponse from "./types/http/IHttpResponse";
+import IHttpRoute from "./types/http/IHttpRoute";
+import IHttpRouterExecutor from "./types/http/IHttpRouterExecutor";
+import INext from "./types/INext";
+import IRouter from "./types/IRouter";
+
+const HTTP_METHODS = ["GET", "PUT", "DELETE", "POST"];
+
+// send an OPTIONS response
+function sendOptionsResponse(res: IHttpResponse, options: string[], next: INext): void {
+  try {
+    const body = options.join(",");
+    res.putHeader("Allow", body);
+    res.send(body);
+  } catch (err) {
+    next(err);
+  }
+}
+
+/**
+ * The objects that describe the layers to apply over path and events. The
+ * routers can be subrouters of other routers. Also, handlers can be set
+ * to be run when an param appears in a path of an incoming HTTP request.
+ */
+export default class Router implements IRouter {
+
+  private _subrouters: IRouter[];
+  private _params: {[name: string]: IHttpPlaceholderHandler[]};
+  private _httpStack: IHttpLayer[];
+  private _eventStack: IEventLayer[];
+  private _caseSensitive: boolean;
+  private _strict: boolean;
+  private _subpath: string;
+  private _parent: IRouter;
+
+  constructor(opts?: {[name: string]: any}) {
+    const options = opts || {};
+
+    this._subrouters = [];
+    this._params = {};
+    this._httpStack = [];
+    this._eventStack = [];
+    this._caseSensitive = options.caseSensitive || false;
+    this._strict = options.strict || false;
+  }
+
+  get subrouters(): IRouter[] {
+    return this._subrouters;
+  }
+
+  get httpStack(): IHttpLayer[] {
+    return this._httpStack;
+  }
+
+  get eventStack(): IEventLayer[] {
+    return this._eventStack;
+  }
+
+  get parent(): IRouter {
+    return this._parent;
+  }
+
+  get subpath(): string {
+    return this._subpath;
+  }
+
+  get fullSubpath(): string {
+    if (!this._subpath || !this._parent) {
+      return this._subpath;
+    } else {
+      let result = null;
+
+      if (this.parent) {
+        const parentFullSubpath = this.parent.fullSubpath;
+        if (parentFullSubpath) {
+          result = parentFullSubpath;
+        }
+      }
+
+      if (!result) {
+        result = "";
+      }
+
+      result = result + this._subpath;
+
+      return result;
+    }
+  }
+
+  public param(name: string, handler: IHttpPlaceholderHandler): IRouter {
+    if (!this._params[name]) {
+      this._params[name] = [];
+    }
+    this._params[name].push(handler);
+
+    return this;
+  }
+
+  public use(handler: IHttpHandler|IHttpHandler[], path?: string): IRouter {
+    let fns: IHttpHandler[];
+    if (Array.isArray(handler)) {
+      fns = handler;
+    } else {
+      fns = [handler];
+    }
+
+    const layers: IHttpLayer[] = fns.map(
+      (fn) => new HttpLayer(
+        this,
+        path,
+        {
+          sensitive: this._caseSensitive,
+          strict: false,
+          end: false
+        },
+        fn
+      )
+  );
+
+    this._httpStack = this._httpStack.concat(layers);
+
+    return this;
+  }
+
+  public mount(router: IRouter, path?: string): IRouter {
+    router.doSubrouter(path, this);
+    this._subrouters.push(router);
+
+    return this;
+  }
+
+  public route(path: string): IHttpRoute {
+    const layer = new HttpLayer(this, path, {
+      sensitive: this._caseSensitive,
+      strict: this._strict,
+      end: true
+    });
+    const route = new HttpRoute(layer);
+    layer.route = route;
+
+    this._httpStack.push(layer);
+
+    return route;
+  }
+
+  public event(event: string|IEventRoutePredicate, handler: IEventHandler): IRouter {
+    const layer = new EventLayer(
+      event,
+      {
+        end: true
+      },
+      handler
+    );
+
+    this._eventStack.push(layer);
+
+    return this;
+  }
+
+  public httpHandle(req: IHttpRequest, res: IHttpResponse, out: INext): void {
+    if (req.method === "OPTIONS") {
+      const options = this.getAvailableMethodsForPath(req.path);
+      if (options.length === 0) {
+        return out();
+      } else {
+        sendOptionsResponse(res, options, out);
+      }
+    } else {
+      const routerExecutor: IHttpRouterExecutor = new HttpRouterExecutor(this, req, res, out);
+      routerExecutor.next();
+    }
+  }
+
+  public doSubrouter(subpath: string, parent: IRouter): void {
+    this._subpath = subpath;
+    this._parent = parent;
+  }
+
+  public httpProcessParams(layerParams: { [name: string]: string }, executedParams: string[], req: IHttpRequest, res: IHttpResponse, done: INext): void {
+    const keys = Object.keys(layerParams);
+    if (keys.length > 0) {
+      const processParam = (index: number) => {
+        const partialDone: INext = (error?: Error) => {
+          if (error) {
+            done(error);
+          } else {
+            const newIndex = index + 1;
+            if (newIndex < keys.length) {
+              processParam(newIndex);
+            } else {
+              done();
+            }
+          }
+        };
+
+        const key = keys[index];
+        const value = layerParams[key];
+        const handlers = this._params[key];
+        if (executedParams.indexOf(key) === -1 && handlers && handlers.length > 0) {
+          const processHandler = (index2: number) => {
+            const partialPartialDone: INext = (error?: Error) => {
+              if (error) {
+                partialDone(error);
+              } else {
+                const newIndex = index2 + 1;
+                if (newIndex < handlers.length) {
+                  processHandler(newIndex);
+                } else {
+                  executedParams.push(key);
+                  partialDone();
+                }
+              }
+            };
+
+            const handler = handlers[index2];
+            handler(req, res, partialPartialDone, value);
+          };
+
+          processHandler(0);
+        } else {
+          partialDone();
+        }
+      };
+
+      processParam(0);
+    } else {
+      done();
+    }
+  }
+
+  public getAvailableMethodsForPath(path: string): string[] {
+    const result: string[] = [];
+
+    this._httpStack.forEach((layer) => {
+      if (layer.route && layer.match(path)) {
+        HTTP_METHODS.forEach((method) => {
+          if (result.indexOf(method) === -1 && layer.route.hasMethod(method)) {
+            result.push(method);
+          }
+        });
+      }
+    });
+
+    this._subrouters.forEach((subrouter) => {
+      subrouter.getAvailableMethodsForPath(path).forEach((method) => {
+        if (result.indexOf(method) === -1) {
+          result.push(method);
+        }
+      });
+    });
+
+    return result;
+  }
+
+  public eventHandle(req: IEventRequest, out: INext): void {
+    const routerExecutor: IEventRouterExecutor = new EventRouterExecutor(this, req, out);
+    routerExecutor.next();
+  }
+
+}
