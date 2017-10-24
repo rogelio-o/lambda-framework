5e2b5c0004ec873d87cadd8e0b94b6a05d015199
rogelio-o <yo@rogelioorts.com>
2017-10-24 20:15:38 +0200

    Added interfaces to exported files.

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,26 @@
  * Copyright(c) 2017 Rogelio Orts
  * MIT Licensed
  */
-import App from "./lib/App";
-import configuration from "./lib/configuration/configuration";
 
-export { configuration };
-export default App;
+export { default } from "./lib/App";
+export { default as configuration } from "./lib/configuration/configuration";
+
+export { default as IApp } from "./lib/types/IApp";
+export { default as INext } from "./lib/types/INext";
+export { default as IRouter } from "./lib/types/IRouter";
+
+export { default as IEventHandler } from "./lib/types/event/IEventHandler";
+export { default as IEventLayer } from "./lib/types/event/IEventLayer";
+export { default as IEventRequest } from "./lib/types/event/IEventRequest";
+export { default as IEventRoutePredicate } from "./lib/types/event/IEventRoutePredicate";
+export { default as IEventRouterExecutor } from "./lib/types/event/IEventRouterExecutor";
+
+export { default as IHttpError } from "./lib/types/exceptions/IHttpError";
+
+export { default as IHttpHandler } from "./lib/types/http/IHttpHandler";
+export { default as IHttpLayer } from "./lib/types/http/IHttpLayer";
+export { default as IHttpPlaceholderHandler } from "./lib/types/http/IHttpPlaceholderHandler";
+export { default as IHttpRequest } from "./lib/types/http/IHttpRequest";
+export { default as IHttpResponse } from "./lib/types/http/IHttpResponse";
+export { default as IHttpRoute } from "./lib/types/http/IHttpRoute";
+export { default as IHttpRouterExecutor } from "./lib/types/http/IHttpRouterExecutor";
