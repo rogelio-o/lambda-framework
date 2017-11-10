82621a9a8d18703d3e52a873ba8eefc0b1738e50
rogelio-o <yo@rogelioorts.com>
2017-11-10 17:41:38 +0100

    Changed render from function to class.

--- src/lib/types/http/renderEngine/ITemplateRenderer.ts
+++ src/lib/types/http/renderEngine/ITemplateRenderer.ts
@@ -1,5 +1,8 @@
 /**
  * The interface that has to implement the function that render a template.
  */
-type ITemplateRenderer = (fileName: string, params: {[name: string]: any}, engineConfiguration: {[name: string]: any}, callback: (err: Error, parsedHtml: string) => void) => void;
-export default ITemplateRenderer;
+export default interface ITemplateRenderer {
+
+  render(fileName: string, params: {[name: string]: any}, engineConfiguration: {[name: string]: any}, callback: (err: Error, parsedHtml: string) => void): void;
+
+}
