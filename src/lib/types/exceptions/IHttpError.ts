d48abfbb4d16b8a355fa5ede89360d1505e468e7
rogelio-o <yo@rogelioorts.com>
2017-11-19 21:49:39 +0100

    Increase HttpResponse test coverture.

--- src/lib/types/exceptions/IHttpError.ts
+++ src/lib/types/exceptions/IHttpError.ts
@@ -1,7 +1,7 @@
 /**
  * An error that happens processing a http request.
  */
-export default interface IHttpError {
+export default interface IHttpError extends Error {
     statusCode: number;
     headers: {[name: string]: string|string[]};
     cause: Error;
--- test/http/HttpResponse.spec.ts
+++ test/http/HttpResponse.spec.ts
@@ -1,5 +1,7 @@
 import * as Chai from 'chai'
+import { sign } from "cookie-signature";
 import { stub, SinonStub } from "sinon";
+import HttpError from "./../../src/lib/exceptions/HttpError";
 import Router from './../../src/lib/Router'
 import IRouter from './../../src/lib/types/IRouter'
 import configuration from './../../src/lib/configuration/configuration'
@@ -99,89 +101,140 @@ describe('HttpResponse', () => {
     Chai.expect(response.statusCode).to.be.equal(302)
   });
 
-  it('#send should set the outcoming response body to the given string with "html" content type header and the right content length header', () => {
-    response.send('ABC')
-    Chai.expect(succResult.headers['Content-Type']).to.be.equal('text/html; charset=utf-8')
-    Chai.expect(succResult.headers['Content-Length']).to.be.equal('3')
-    Chai.expect(response.isSent).to.be.true
+  describe("#send", () => {
+    it('should set the outcoming response body to the given string with "html" content type header and the right content length header', () => {
+      response.send('ABC')
+      Chai.expect(succResult.headers['Content-Type']).to.be.equal('text/html; charset=utf-8')
+      Chai.expect(succResult.headers['Content-Length']).to.be.equal('3')
+      Chai.expect(response.isSent).to.be.true
+    });
+
+    it('should set the outcoming response body to the given number with no content type header and the right content length header', () => {
+      response.send(false)
+      Chai.expect(succResult.headers['Content-Type']).to.be.undefined
+      Chai.expect(succResult.headers['Content-Length']).to.be.equal('5')
+      Chai.expect(succResult.body).to.be.equal('false')
+      Chai.expect(response.isSent).to.be.true
+    });
+
+    it('should set the outcoming response body to the given boolean with no content type header and the right content length header', () => {
+      response.send(1)
+      Chai.expect(succResult.headers['Content-Type']).to.be.undefined
+      Chai.expect(succResult.headers['Content-Length']).to.be.equal('1')
+      Chai.expect(succResult.body).to.be.equal('1')
+      Chai.expect(response.isSent).to.be.true
+    });
+
+    it('should set the outcoming response body to the given Buffer with "bin" content type header and the right content length header', () => {
+      response.send(new Buffer('test'))
+      Chai.expect(succResult.headers['Content-Type']).to.be.equal('bin')
+      Chai.expect(succResult.headers['Content-Length']).to.be.equal('4')
+      Chai.expect(succResult.body).to.be.equal('test')
+      Chai.expect(response.isSent).to.be.true
+    });
+
+    it('should set the outcoming response header ETag to the generated with the function in #app.get(Configuration.ETAG_FN)', () => {
+      app.set(configuration.ETAG_FN, (buff, encoding) => 'ETAG' + buff.toString(encoding))
+      response.send('test')
+      Chai.expect(succResult.headers['ETag']).to.be.equal('ETAGtest')
+    });
+
+    it('should set the outcoming response status code to 304 if the response is not fresh', () => {
+      app.set(configuration.ETAG_FN, (buff, encoding) => 'etagValue')
+      response.putHeader('Last-Modified', '2017-10-10T10:10:09')
+      response.send('test')
+      Chai.expect(succResult.statusCode).to.be.equal(304)
+    });
+
+    it('should not set the outcoming response irrelevant headers (Content-Type, Content-Length and Transfer-Encoding) and the body if the response status code is 204', () => {
+      response.status(204)
+      response.send('')
+      Chai.expect(succResult.headers['Content-Type']).to.be.undefined
+      Chai.expect(succResult.headers['Content-Length']).to.be.undefined
+      Chai.expect(succResult.headers['Transfer-Encoding']).to.be.undefined
+    });
+
+    it('should not set the outcoming response irrelevant headers (Content-Type, Content-Length and Transfer-Encoding) and the body if the response status code is 304', () => {
+      response.status(304)
+      response.send('')
+      Chai.expect(succResult.headers['Content-Type']).to.be.undefined
+      Chai.expect(succResult.headers['Content-Length']).to.be.undefined
+      Chai.expect(succResult.headers['Transfer-Encoding']).to.be.undefined
+    });
+
+    it('should set the outcoming response body to null if the request method is HEAD', () => {
+      event.httpMethod = 'HEAD'
+      response.send('test')
+      Chai.expect(succResult.body).to.be.undefined
+    });
+
+    it("should set content type to bin if the sent body is a buffer and there is no previous set content type into the response.", () => {
+      response.send(new Buffer(1));
+      Chai.expect(succResult.headers["Content-Type"]).to.be.equal("bin");
+    });
+
+    it("should no set the conent-length header.", () => {
+      response.send(undefined);
+      Chai.expect(succResult.headers["Content-Length"]).to.be.undefined;
+    });
+
+    it("should generate a etag and put it into the response header if there is a generator function configurated in the app.", () => {
+      app.set(configuration.ETAG_FN, (buff, encoding) => {
+        return buff.toString() + "-" + encoding;
+      });
+
+      response.send("test");
+      Chai.expect(succResult.headers["ETag"]).to.be.equal("test-undefined");
+    });
+
+    it("should pass the error param to the callback function if an error is set.", () => {
+      const err = new HttpError("Test error.", 500);
+      response.setError(err);
+      response.send("");
+      Chai.expect(errResult).to.be.equal(err);
+    });
+
+    it("should pass the error param with the cause to the callback function if an error is set and it has a cause.", () => {
+      const cause = new Error("Cause error.");
+      const err = new HttpError("Test error.", 500, cause);
+      response.setError(err);
+      response.send("");
+      Chai.expect(errResult).to.be.equal(cause);
+    });
   });
 
-  it('#send should set the outcoming response body to the given number with no content type header and the right content length header', () => {
-    response.send(false)
-    Chai.expect(succResult.headers['Content-Type']).to.be.undefined
-    Chai.expect(succResult.headers['Content-Length']).to.be.equal('5')
-    Chai.expect(succResult.body).to.be.equal('false')
-    Chai.expect(response.isSent).to.be.true
+  describe("#json", () => {
+    it('should set the outcoming response header Content-Type to "application/json"', () => {
+      response.json({})
+      Chai.expect(succResult.headers['Content-Type']).to.be.equal('application/json; charset=utf-8')
+    });
+
+    it("should keep the previous set content type if exists.", () => {
+      response.contentType("text/plain");
+      response.json({})
+      Chai.expect(succResult.headers["Content-Type"]).to.be.equal("text/plain; charset=utf-8")
+    });
+
+    it('should set the outcoming response header Content-Length to the right length', () => {
+      const body = {test: 'test1'}
+      const parsedBody = stringify(body, undefined, undefined, undefined)
+      response.json(body)
+      Chai.expect(succResult.headers['Content-Length']).to.be.equal(parsedBody.length.toString())
+      Chai.expect(succResult.body).to.be.equal(parsedBody)
+    });
   });
 
-  it('#send should set the outcoming response body to the given boolean with no content type header and the right content length header', () => {
-    response.send(1)
-    Chai.expect(succResult.headers['Content-Type']).to.be.undefined
-    Chai.expect(succResult.headers['Content-Length']).to.be.equal('1')
-    Chai.expect(succResult.body).to.be.equal('1')
-    Chai.expect(response.isSent).to.be.true
-  });
+  describe("#sendStatus", () => {
+    it('should set the outcoming response header Content-Length to "txt", the body to the status code body and the satus code to the given status code', () => {
+      response.sendStatus(404)
+      Chai.expect(succResult.headers['Content-Type']).to.be.equal('text/plain; charset=utf-8')
+      Chai.expect(succResult.body).to.be.equal('Not Found')
+    });
 
-  it('#send should set the outcoming response body to the given Buffer with "bin" content type header and the right content length header', () => {
-    response.send(new Buffer('test'))
-    Chai.expect(succResult.headers['Content-Type']).to.be.equal('bin')
-    Chai.expect(succResult.headers['Content-Length']).to.be.equal('4')
-    Chai.expect(succResult.body).to.be.equal('test')
-    Chai.expect(response.isSent).to.be.true
-  });
-
-  it('#send should set the outcoming response header ETag to the generated with the function in #app.get(Configuration.ETAG_FN)', () => {
-    app.set(configuration.ETAG_FN, (buff, encoding) => 'ETAG' + buff.toString(encoding))
-    response.send('test')
-    Chai.expect(succResult.headers['ETag']).to.be.equal('ETAGtest')
-  });
-
-  it('#send should set the outcoming response status code to 304 if the response is not fresh', () => {
-    app.set(configuration.ETAG_FN, (buff, encoding) => 'etagValue')
-    response.putHeader('Last-Modified', '2017-10-10T10:10:09')
-    response.send('test')
-    Chai.expect(succResult.statusCode).to.be.equal(304)
-  });
-
-  it('#send should not set the outcoming response irrelevant headers (Content-Type, Content-Length and Transfer-Encoding) and the body if the response status code is 204', () => {
-    response.status(204)
-    response.send('')
-    Chai.expect(succResult.headers['Content-Type']).to.be.undefined
-    Chai.expect(succResult.headers['Content-Length']).to.be.undefined
-    Chai.expect(succResult.headers['Transfer-Encoding']).to.be.undefined
-  });
-
-  it('#send should not set the outcoming response irrelevant headers (Content-Type, Content-Length and Transfer-Encoding) and the body if the response status code is 304', () => {
-    response.status(304)
-    response.send('')
-    Chai.expect(succResult.headers['Content-Type']).to.be.undefined
-    Chai.expect(succResult.headers['Content-Length']).to.be.undefined
-    Chai.expect(succResult.headers['Transfer-Encoding']).to.be.undefined
-  });
-
-  it('#send should set the outcoming response body to null if the request method is HEAD', () => {
-    event.httpMethod = 'HEAD'
-    response.send('test')
-    Chai.expect(succResult.body).to.be.undefined
-  });
-
-  it('#json should set the outcoming response header Content-Type to "application/json"', () => {
-    response.json({})
-    Chai.expect(succResult.headers['Content-Type']).to.be.equal('application/json; charset=utf-8')
-  });
-
-  it('#json should set the outcoming response header Content-Length to the right length', () => {
-    const body = {test: 'test1'}
-    const parsedBody = stringify(body, undefined, undefined, undefined)
-    response.json(body)
-    Chai.expect(succResult.headers['Content-Length']).to.be.equal(parsedBody.length.toString())
-    Chai.expect(succResult.body).to.be.equal(parsedBody)
-  });
-
-  it('#sendStatus should set the outcoming response header Content-Length to "txt", the body to the status code body and the satus code to the given status code', () => {
-    response.sendStatus(404)
-    Chai.expect(succResult.headers['Content-Type']).to.be.equal('text/plain; charset=utf-8')
-    Chai.expect(succResult.body).to.be.equal('Not Found')
+    it("should set the status code as response body if there is no message for it.", () => {
+      response.sendStatus(1)
+      Chai.expect(succResult.body).to.be.equal("1");
+    });
   });
 
   it('#contentType should set content type to the given one', () => {
@@ -281,11 +334,31 @@ describe('HttpResponse', () => {
     Chai.expect(response.header('field')).to.be.equal('value')
   });
 
-  it('#appendHeader should append a header to a previously set header', () => {
-    response.putHeader('field', 'value1');
-    response.appendHeader('field', 'value2');
-    Chai.expect(response.header('field')).to.be.contain('value1')
-    Chai.expect(response.header('field')).to.be.contain('value2')
+  describe("#appendHeader", () => {
+    it('should append a header to a previously set header', () => {
+      response.putHeader('field', 'value1');
+      response.appendHeader('field', 'value2');
+      Chai.expect(response.header('field')).to.be.contain('value1')
+      Chai.expect(response.header('field')).to.be.contain('value2')
+    });
+
+    it("should add append a header to a list of headers previously set.", () => {
+      response.putHeader("field", ["value1", "value2"]);
+      response.appendHeader("field", "value3");
+      Chai.expect(response.header("field")).to.be.deep.equal(["value1", "value2", "value3"]);
+    });
+
+    it("should add append a list of headers to a list of headers previously set.", () => {
+      response.putHeader("field", ["value1", "value2"]);
+      response.appendHeader("field", ["value3", "value4"]);
+      Chai.expect(response.header("field")).to.be.deep.equal(["value1", "value2", "value3", "value4"]);
+    });
+
+    it("should add append a list of headers to a header previously set.", () => {
+      response.putHeader("field", "value1");
+      response.appendHeader("field", ["value2", "value3"]);
+      Chai.expect(response.header("field")).to.be.deep.equal(["value1", "value2", "value3"]);
+    });
   });
 
   it('#removeHeader should remove a previously set header', () => {
@@ -294,9 +367,43 @@ describe('HttpResponse', () => {
     Chai.expect(response.header('field')).to.be.undefined
   });
 
-  it('#addCookie should add the `name` cookie with the `value` and the `options`', () => {
-    response.addCookie('cookie', 'cookieValue', {path: '/test'})
-    Chai.expect(response.header('Set-Cookie')).to.be.equal('cookie=cookieValue; Path=/test')
+  describe("#addCookie", () => {
+    it('should add the `name` cookie with the `value` and the `options`', () => {
+      response.addCookie('cookie', 'cookieValue', {path: '/test'})
+      Chai.expect(response.header('Set-Cookie')).to.be.equal('cookie=cookieValue; Path=/test')
+    });
+
+    it("should throw an error if the option `signed` is true and the app has no `secret`.", () => {
+      app.set(configuration.COOKIE_SECRET, null);
+      Chai.expect(() => response.addCookie("cookie", "cookieValue", {signed: true})).to.throw("app.set(\"cookie_secret\", \"SECRET\") required for signed cookies.");
+    });
+
+    it("should stringify the value of the cookie if is an object.", () => {
+      response.addCookie("cookie", {key: "value"});
+      Chai.expect(response.header("Set-Cookie")).to.be.equal("cookie=" + encodeURIComponent("j:{\"key\":\"value\"}") + "; Path=/");
+    });
+
+    it("should sign the value with the secret if the option `signed` is true.", () => {
+      app.set(configuration.COOKIE_SECRET, "SUPER_SECRET");
+      response.addCookie("cookie", "cookieValue", {signed: true});
+      Chai.expect(response.header("Set-Cookie")).to.be.equal("cookie=" + encodeURIComponent("s:" + sign("cookieValue", app.get(configuration.COOKIE_SECRET))) + "; Path=/");
+    });
+
+    it("should add `expires` and `maxAge` into the cookie if `maxAge` is given in options.", () => {
+      var oldDate = Date;
+      global.Date = new Proxy(Date, {
+        construct: function(target, args) {
+          return new oldDate(2017, 11, 19, 0, 0, 0, 0);
+        }
+      });
+
+      var x = new Date();
+      response.addCookie("cookie", "cookieValue", {maxAge: 5000}); // 5 seconds
+      const expires = new Date(Date.now() + 5000);
+      Chai.expect(response.header("Set-Cookie")).to.be.equal("cookie=cookieValue; Max-Age=5; Path=/; Expires=" + expires.toUTCString());
+
+      global.Date = oldDate;
+    });
   });
 
   it('#addCookies should add all the cookies of `obj` with the `options`', () => {
@@ -321,21 +428,62 @@ describe('HttpResponse', () => {
     Chai.expect(response.cookie('cookie1')).to.be.equal('cookieValue1')
   });
 
-  it('#location should set the header Location to the given URL', () => {
-    response.location('/test');
-    Chai.expect(response.header('Location')).to.be.equal('/test')
+  describe("location", () => {
+    it('should set the header Location to the given URL', () => {
+      response.location('/test');
+      Chai.expect(response.header('Location')).to.be.equal('/test')
+    });
+
+    it("should set the header Location to the Referrer URL if it exists and the given URL is \"back\".", () => {
+      request.headers["referrer"] = "/previous-url";
+      response.redirect("back");
+      Chai.expect(succResult.headers["Location"]).to.be.equal("/previous-url");
+    });
+
+    it("should set the header Location to / if the Referrer URL does not exist and the given URL is \"back\".", () => {
+      response.redirect("back");
+      Chai.expect(succResult.headers["Location"]).to.be.equal("/");
+    });
   });
 
-  it('#redirect should set the header Location to the given URL and the status code to 302', () => {
-    response.redirect('/test');
-    Chai.expect(succResult.headers['Location']).to.be.equal('/test')
-    Chai.expect(succResult.statusCode).to.be.equal(302)
-  });
+  describe("#redirect", () => {
+    it('should set the header Location to the given URL and the status code to 302', () => {
+      response.redirect('/test');
+      Chai.expect(succResult.headers['Location']).to.be.equal('/test')
+      Chai.expect(succResult.statusCode).to.be.equal(302)
+    });
 
-  it('#redirect should set the header Location to the given URL and the status code to the given one', () => {
-    response.redirect('/test', 301);
-    Chai.expect(succResult.headers['Location']).to.be.equal('/test')
-    Chai.expect(succResult.statusCode).to.be.equal(301)
+    it('should set the header Location to the given URL and the status code to the given one', () => {
+      response.redirect('/test', 301);
+      Chai.expect(succResult.headers['Location']).to.be.equal('/test')
+      Chai.expect(succResult.statusCode).to.be.equal(301)
+    });
+
+    it("should set the body to HTML text if the request accepts text/html.", () => {
+      request.headers["accept"] = "text/html";
+      response.redirect("/test");
+      Chai.expect(succResult.body).to.be.equal("<p>Found. Redirecting to <a href=\"/test\">/test</a></p>");
+    });
+
+    it("should set the body to plain text if the request accepts text/plain.", () => {
+      request.headers["accept"] = "text/plain";
+      response.redirect("/test");
+      Chai.expect(succResult.body).to.be.equal("Found. Redirecting to /test.");
+    });
+
+    it("should set the body to empty string if the request doesn't accepts neither text/html nor text/plain.", () => {
+      request.headers["accept"] = "application/json";
+      response.redirect("/test");
+      Chai.expect(succResult.body).to.be.equal("");
+    });
+
+    it("should set the body to empty string, 200 status code, and no Location header if the request HTTP method is HEAD.", () => {
+      request.event.httpMethod = "HEAD";
+      response.redirect("/test");
+      Chai.expect(succResult.body).to.be.undefined;
+      Chai.expect(succResult.headers["Location"]).to.be.equal("/test");
+      Chai.expect(succResult.statusCode).to.be.equal(302);
+    });
   });
 
   describe("render", () => {
